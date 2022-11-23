0068a17a7ef29de588bd714585a9698390a6e9cc
Lisa Courtade <lisa.courtade@hotmail.fr>
2022-11-23 18:46:26 +0200

    added templates for seasons and for episodes

--- index.js
+++ index.js
@@ -33,6 +33,35 @@ app.get('/tv-shows/:id', async (req, res) => {
 })
 
 
+const getSeasons = async function (showId) {
+    try {
+        const result = await axios.get(`https://api.tvmaze.com/shows/${showId}/seasons`);
+        return result.data;
+    } catch (e) {
+        console.log(e); 
+    }
+}
+
+app.get('/tv-shows/:id/seasons', async (req, res) => {
+    const { id } = req.params;
+    seasonsData = await getSeasons(id)
+    res.render('seasons.ejs', { seasonsData } );    
+})
+
+const getEpisodes = async function (seasonId) {
+    try {
+        const result = await axios.get(`https://api.tvmaze.com/seasons/${seasonId}/episodes`);
+        return result.data;
+    } catch (e) {
+        console.log(e); 
+    }
+}
+
+app.get('/tv-shows/:id/episodes', async (req, res) => {
+    const { id } = req.params;
+    seasonData = await getEpisodes(id)
+    res.render('episodes.ejs', { seasonData } );    
+})
 
 
 app.listen(3000, () => {
